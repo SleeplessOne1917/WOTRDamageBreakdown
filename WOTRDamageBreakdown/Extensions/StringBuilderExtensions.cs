93a3907c5cbba22192ff34393a3081cb9dc3f9d4
abias <abias1122@gmail.com>
2022-02-06 06:48:15 -0500

    Add extra condition to ability score

--- WOTRDamageBreakdown/Extensions/StringBuilderExtensions.cs
+++ WOTRDamageBreakdown/Extensions/StringBuilderExtensions.cs
@@ -66,7 +66,7 @@ namespace WOTRDamageBreakdown.Extensions
                     string source;
                     var fact = modifiers[i].Fact;
 
-                    if (i == 0 && damageBonusStat.HasValue && modifiers[i].Descriptor == ModifierDescriptor.None)
+                    if (i == 0 && damageBonusStat.HasValue && modifiers[i].Descriptor == ModifierDescriptor.None && !fact.GetName().Contains("Weapon Training"))
                     {
                         source = damageBonusStat.Value.ToString();
                     }
