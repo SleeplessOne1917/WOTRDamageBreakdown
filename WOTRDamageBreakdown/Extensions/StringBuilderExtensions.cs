c141f26b09f469e57e1f51091381aa6470acecd0
abias <abias1122@gmail.com>
2022-02-07 19:54:20 -0500

    Revert "Get modifiers before calling append in case they cancel eachother out"
    This reverts commit debef1213b5e0cd3c797131d0c06609236db5428.

--- WOTRDamageBreakdown/Extensions/StringBuilderExtensions.cs
+++ WOTRDamageBreakdown/Extensions/StringBuilderExtensions.cs
@@ -1,8 +1,11 @@
-﻿using Kingmaker.Enums;
+﻿using Kingmaker.EntitySystem.Stats;
+using Kingmaker.Enums;
 using Kingmaker.RuleSystem.Rules;
 using Kingmaker.RuleSystem.Rules.Damage;
 using Kingmaker.UI.Common;
-using System.Collections.Generic;
+using Kingmaker.UnitLogic.Parts;
+using Kingmaker.Utility;
+using System;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -11,10 +14,48 @@ namespace WOTRDamageBreakdown.Extensions
 {
     public static class StringBuilderExtensions
     {
-        public static void AppendDamageModifiersBreakdown(this StringBuilder sb, RuleDealDamage rule, List<Modifier> modifiers)
+        public static void AppendDamageModifiersBreakdown(this StringBuilder sb, RuleDealDamage rule, int totalBonus, int trueTotal)
         {
+            var modifiers = rule.ResultList.First().Source.Modifiers;
             var weapon = rule.DamageBundle.Weapon;
             var damageBonusStat = rule.AttackRoll?.WeaponStats.DamageBonusStat;
+            DamageType damageType = rule.ResultList.First().Source.Type;
+
+            var additionalDamageModifiers = rule.Initiator.Stats.AdditionalDamage.Modifiers;
+            if ((!rule.SourceAbility?.IsSpell ?? true) && (!rule.SourceAbility?.IsCantrip ?? true) && additionalDamageModifiers.Count() > 0)
+            {
+                var stackableModifiers = additionalDamageModifiers
+                    .Where(m => m.Stacks)
+                    .Select(MapModifier);
+                var nonStackableModifiers = additionalDamageModifiers
+                    .Where(m => !m.Stacks)
+                    .GroupBy(m => m.ModDescriptor)
+                    .Select(g => MapModifier(g.MaxBy(m => m.ModValue)));
+
+                modifiers.AddRange(stackableModifiers);
+                modifiers.AddRange(nonStackableModifiers);
+
+                totalBonus += stackableModifiers.Sum(m => m.Value) + nonStackableModifiers.Sum(m => m.Value);
+            }
+
+            if (totalBonus != trueTotal)
+            {
+                var unitPartWeaponTraining = rule.Initiator.Descriptor.Get<UnitPartWeaponTraining>();
+                var weaponTrainingRank = unitPartWeaponTraining?.GetWeaponRank(weapon);
+                if (weaponTrainingRank.HasValue && weaponTrainingRank.Value > 0)
+                {
+                    modifiers.Add(new Modifier(weaponTrainingRank.Value, unitPartWeaponTraining.WeaponTrainings.First(), ModifierDescriptor.None));
+                    totalBonus += weaponTrainingRank.Value;
+                }
+            }
+
+            if (totalBonus != trueTotal)
+                modifiers.Add(new Modifier(trueTotal - totalBonus, ModifierDescriptor.UntypedStackable));
+
+            if (modifiers.Count <= 0)
+                return;
+
+            modifiers.Sort(new Comparison<Modifier>(CompareModifiers));
 
             for (var i = 0; i < modifiers.Count; ++i)
             {
@@ -64,6 +105,16 @@ namespace WOTRDamageBreakdown.Extensions
                     sb.AppendBonus(modifiers[i].Value, source, modifiers[i].Descriptor);
                 }
             }
+        }
+
+        private static int CompareModifiers(Modifier x, Modifier y)
+        {
+            return ModifierDescriptorComparer.Instance.Compare(x.Descriptor, y.Descriptor);
+        }
+
+        private static Modifier MapModifier(ModifiableValue.Modifier mod)
+        {
+            return new Modifier(mod.ModValue, mod.Source, mod.ModDescriptor);
         }
     }
 }
--- WOTRDamageBreakdown/Patches/DamageLogPatch.cs
+++ WOTRDamageBreakdown/Patches/DamageLogPatch.cs
@@ -1,13 +1,7 @@
 ﻿using HarmonyLib;
 using Kingmaker.Blueprints.Root.Strings.GameLog;
-using Kingmaker.EntitySystem.Stats;
-using Kingmaker.Enums;
-using Kingmaker.RuleSystem.Rules;
 using Kingmaker.RuleSystem.Rules.Damage;
-using Kingmaker.UnitLogic.Parts;
 using Kingmaker.Utility;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using WOTRDamageBreakdown.Extensions;
@@ -23,69 +17,17 @@ namespace WOTRDamageBreakdown.Patches
                 return;
 
             var firstDamage = rule.ResultList.Select(damageValue => damageValue.Source).First();
-            int totalBonus = firstDamage.TotalBonus;
+            var totalBonus = firstDamage.Modifiers.Sum(m => m.Value);
+            int trueTotal = firstDamage.TotalBonus;
             var dice = firstDamage.Dice;
             var isZeroDice = dice.Dice == Kingmaker.RuleSystem.DiceType.Zero || dice.Rolls == 0;
-            var modifiers = GetModifiers(rule, totalBonus, firstDamage).ToList();
 
-            if (modifiers.Count > 0 && !isZeroDice)
+            if (trueTotal != 0 && !isZeroDice)
             {
                 sb.AppendLine();
-                sb.AppendLine($"<b>Damage bonus: {UIConsts.GetValueWithSign(totalBonus)}</b>");
-                sb.AppendDamageModifiersBreakdown(rule, modifiers);
+                sb.AppendLine($"<b>Damage bonus: {UIConsts.GetValueWithSign(trueTotal)}</b>");
+                sb.AppendDamageModifiersBreakdown(rule, totalBonus, trueTotal);
             }
-        }
-
-        private static IEnumerable<Modifier> GetModifiers(RuleDealDamage rule, int trueTotal, BaseDamage damage)
-        {
-            var modifiers = damage.Modifiers;
-            var totalBonus = modifiers.Sum(m => m.Value);
-
-            var additionalDamageModifiers = rule.Initiator.Stats.AdditionalDamage.Modifiers;
-            if ((!rule.SourceAbility?.IsSpell ?? true) && (!rule.SourceAbility?.IsCantrip ?? true) && additionalDamageModifiers.Count() > 0)
-            {
-                var stackableModifiers = additionalDamageModifiers
-                    .Where(m => m.Stacks)
-                    .Select(MapModifier);
-                var nonStackableModifiers = additionalDamageModifiers
-                    .Where(m => !m.Stacks)
-                    .GroupBy(m => m.ModDescriptor)
-                    .Select(g => MapModifier(g.MaxBy(m => m.ModValue)));
-
-                modifiers.AddRange(stackableModifiers);
-                modifiers.AddRange(nonStackableModifiers);
-
-                totalBonus += stackableModifiers.Sum(m => m.Value) + nonStackableModifiers.Sum(m => m.Value);
-            }
-
-            if (totalBonus != trueTotal)
-            {
-                var weapon = rule.DamageBundle.Weapon;
-                var unitPartWeaponTraining = rule.Initiator.Descriptor.Get<UnitPartWeaponTraining>();
-                var weaponTrainingRank = unitPartWeaponTraining?.GetWeaponRank(weapon);
-                if (weaponTrainingRank.HasValue && weaponTrainingRank.Value > 0)
-                {
-                    modifiers.Add(new Modifier(weaponTrainingRank.Value, unitPartWeaponTraining.WeaponTrainings.First(), ModifierDescriptor.None));
-                    totalBonus += weaponTrainingRank.Value;
-                }
-            }
-
-            if (totalBonus != trueTotal)
-                modifiers.Add(new Modifier(trueTotal - totalBonus, ModifierDescriptor.UntypedStackable));
-
-            modifiers.Sort(new Comparison<Modifier>(CompareModifiers));
-
-            return modifiers;
-        }
-
-        private static int CompareModifiers(Modifier x, Modifier y)
-        {
-            return ModifierDescriptorComparer.Instance.Compare(x.Descriptor, y.Descriptor);
-        }
-
-        private static Modifier MapModifier(ModifiableValue.Modifier mod)
-        {
-            return new Modifier(mod.ModValue, mod.Source, mod.ModDescriptor);
         }
     }
 }
